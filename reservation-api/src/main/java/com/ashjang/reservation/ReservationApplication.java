ff7e71c621b217db3d65f47047a4a3b84ac660b4
ashjang <vkdvkdgkfla@naver.com>
2023-06-03 22:23:08 +0900

    feat: [RESERVATION-API] 예약 모듈 생성 및 세팅

---
+++ reservation-api/src/main/java/com/ashjang/reservation/ReservationApplication.java
@@ -0,0 +1,20 @@
+package com.ashjang.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@ServletComponentScan
+@EnableJpaAuditing
+@EnableFeignClients
+public class ReservationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationApplication.class, args);
+    }
+}
